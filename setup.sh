312ffeb89bae3238ef5f7341526085da0c5c3613
Tommaso Dossi <doxeno.2003@gmail.com>
2024-06-12 16:47:53 +0200

    better setup and benchmarking

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #! /bin/sh
 
-#TODO: everything in here is outdated
-
-cd data/facebook
-
+cd benchmarks
 wget https://snap.stanford.edu/data/facebook_combined.txt.gz
 gzip -d facebook_combined.txt.gz
-
-./create_matrix.py facebook_combined.txt 4039 > facebook.mat
+../data/create_matrix.py facebook_combined.txt 4039 > facebook.mat
 rm facebook_combined.txt
 
-cd ../adhoc
+# default settings
+../data/adhoc.py 200 adhoc_200
+../data/adhoc.py 300 adhoc_300
+../data/adhoc.py 400 adhoc_400
+../data/adhoc.py 500 adhoc_500
 
-./adhoc.py 200 adhoc_200
-./adhoc.py 300 adhoc_300
-./adhoc.py 400 adhoc_400
-./adhoc.py 500 adhoc_500
+# high noise
+../data/adhoc.py 200 noise_200 0 1 2 10
+../data/adhoc.py 300 noise_300 0 1 2 10
+../data/adhoc.py 400 noise_400 0 1 2 10
+../data/adhoc.py 500 noise_500 0 1 2 10
 
-cd ../..
+cd ..
+
+make -C src/heuristics
