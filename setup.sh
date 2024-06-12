c699341a783d6f066842d0ac682ac16e135f2942
Tommaso Dossi <doxeno.2003@gmail.com>
2024-06-12 16:52:26 +0200

    added new testcases

--- setup.sh
+++ setup.sh
@@ -18,6 +18,15 @@ rm facebook_combined.txt
 ../data/adhoc.py 400 noise_400 0 1 2 10
 ../data/adhoc.py 500 noise_500 0 1 2 10
 
+# small random matrices
+../data/random_matrix.py random_10 10
+../data/random_matrix.py random_11 11
+../data/random_matrix.py random_12 12
+../data/random_matrix.py random_14 14
+../data/random_matrix.py random_16 16
+../data/random_matrix.py random_18 18
+../data/random_matrix.py random_20 20
+
 cd ..
 
 make -C src/heuristics
