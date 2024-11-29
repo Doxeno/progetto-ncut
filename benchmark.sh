748167d23d379e399ed3de8b7c5fd48e69602b60
Tommaso Dossi <doxeno.2003@gmail.com>
2024-11-30 00:11:03 +0100

    reorder output

--- benchmark.sh
+++ benchmark.sh
@@ -7,19 +7,19 @@ testname="benchmarks/"$1
 cluster_file=$1"._cluster.txt"
 
 if [[ -e $testname".mat" && -z $2 ]]; then
-    src/heuristics/greedy $testname
-    echo
-    src/heuristics/bruteforce $testname
-    echo
-    src/heuristics/shuffle $testname
-    echo
-    src/main.m $testname
     if [ -e $testname"_cluster.txt" ]; then
         echo -e "Ad-hoc matrix, estimated best nCut:"
         cat $testname"_adhoc_res.txt"
         echo -e "Diff: "
         diff --color $testname"_cluster.txt" $testname"_res.mat"
     fi
+    src/heuristics/bruteforce $testname
+    echo
+    src/main.m $testname
+    echo
+    src/heuristics/greedy $testname
+    echo
+    src/heuristics/shuffle $testname
 elif [ -z $1 ]; then
     echo -e "$usage"
 elif [ -z $2 ]; then
